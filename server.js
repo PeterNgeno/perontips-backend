dc2a64a657c5ec34a0138964e64bb1c8ec257de7
PeterNgeno <peterkipsangngeno@gmail.com>
2025-03-06 07:43:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const {
 // Generate access token
 async function getAccessToken() {
     const auth = Buffer.from(`${DARAJA_CONSUMER_KEY}:${DARAJA_CONSUMER_SECRET}`).toString('base64');
-    const response = await axios.get('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
+    const response = await axios.get('https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials', {
         headers: { Authorization: `Basic ${auth}` }
     });
     return response.data.access_token;
